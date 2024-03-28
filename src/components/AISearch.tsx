45be8211927473fe22eada0667ba67b2a0e827d8
MarkCodering <mark.chen.sstm@gmail.com>
2024-03-28 10:07:20 +0800

    Update AISearch.tsx to use default value for anon_key

--- src/components/AISearch.tsx
+++ src/components/AISearch.tsx
@@ -1,7 +1,8 @@
 import {MendableSearchBar} from '@mendable/search';
 
 // Read the açpi key from the environment variables set in the .env file or server's environment
-const anon_key = process.env.MENDABLE_KEY;
+const anon_key =
+  process.env.MENDABLE_KEY || '95c86635-583e-42d5-ac07-b000741a1635';
 
 export default function Search() {
   // @ts-ignore
